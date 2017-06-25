55fd99db1500320935aa441beb70982a9c1c09f7
Łukasz.Rojszczak <lukasz.rojszczak@gmail.com>
2017-06-25 23:19:38 +0200

    Dodano dwa kolejne przypadki testowe

--- src/test/java/lab2_1/Test1.java
+++ src/test/java/lab2_1/Test1.java
@@ -8,29 +8,46 @@ import static org.hamcrest.CoreMatchers.*;
 
 public class Test1 {
 
-	private int key;
-	private int[] seq;
+	private int keyFirstInSeq;
+	private int keyNotInSeq;
+	private int[] seqWithOneElement;
+	private int[] seqWithFewElements;
 	
 	@Before
 	public void init(){
-		key=1;
-		seq = new int[] {1};
+		keyFirstInSeq=1;
+		keyNotInSeq=11;
+		seqWithOneElement = new int[] {1};
+		seqWithFewElements = new int[] {1, 2, 3, 4, 5};
 	}
 	
 	@After
 	public void after() throws IllegalArgumentException{
 		
-		if(seq.length <= 0)
+		if(seqWithOneElement.length <= 0 || seqWithFewElements.length <= 0  )
 			throw new IllegalArgumentException("D³ugoœæ sekwencji wejœciowej nie mo¿e byæ równy 0");
 	}
 	
 	@Test
-	public void test() throws IllegalArgumentException{
+	public void testKeyInOneElementSeq(){
+		SearchResult searchResult = BinarySearch.search(keyFirstInSeq, seqWithOneElement);
+		assertThat(searchResult.getPosition(), equalTo(0));
+		assertThat(searchResult.isFound(), is(true));
+	}
 
-		SearchResult searchResult = BinarySearch.search(key, seq);
+	@Test
+	public void testKeyNotInOneElementSeq(){
+		SearchResult searchResult = BinarySearch.search(keyNotInSeq, seqWithOneElement);
+		assertThat(searchResult.getPosition(), equalTo(-1));
+		assertThat(searchResult.isFound(), is(false));
+	}
+	
+	@Test
+	public void testKeyFirstInFewElemetSeq(){
+		SearchResult searchResult = BinarySearch.search(keyFirstInSeq, seqWithFewElements);
 		assertThat(searchResult.getPosition(), equalTo(0));
 		assertThat(searchResult.isFound(), is(true));
 		
 	}
-
+	
 }
